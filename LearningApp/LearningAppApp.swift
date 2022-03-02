ed0f03ed9cb2b2444a519cbb77379d44e4231681
Gokhan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-03-02 20:26:53 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Gokhan Bozkurt on 2.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
