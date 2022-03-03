fb840a6218ed94ea535a05eabf74cd9e3be30b81
Gokhan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-03-03 15:27:43 +0300

    -Decode Jsonfile for data - Create various models at Models.swift -Decode style html file -Add image assests

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -7,6 +7,38 @@
 
 import Foundation
 
-class ContentModel: ObservableObject,Decodable {
+class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData:Data?
+    
+    init() {
+        getLocalData()
+    }
+    func getLocalData() {
+        // get url path to the json file
+       //   let pathString = Bundle.main.path(forResource: "recipes", ofType: "json")
+       let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+       
+        do {
+           let jsonData = try Data(contentsOf: jsonUrl!)
+           let jsonDecoder = JSONDecoder()
+          let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+          // Assing parsed modeles to modules property
+           self.modules = modules
+       }
+       catch {
+          print(error)
+       }
+       // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        }
+        catch {
+            print("error")
+        }
+   }
 }
